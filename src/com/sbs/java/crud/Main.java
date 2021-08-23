83cad7f844c71b7c9ab433752bb12fc91fd07923
PGVAN <javavandi07@gmail.com>
2021-08-23 18:54:07 +0900

    210823 목록구현

--- src/com/sbs/java/crud/Main.java
+++ src/com/sbs/java/crud/Main.java
@@ -1,5 +1,7 @@
 package com.sbs.java.crud;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,43 +9,71 @@ public class Main {
 		System.out.println("== 프로그램 시작 ==");
 		Scanner sc = new Scanner(System.in);
 		int lastArticleId = 0;
-		
-		while(true) {
-			
+
+		List<Article> articles = new ArrayList<>();
+
+		while (true) {
+
 			String command = sc.nextLine().trim(); // 앞쪽 공백 처리
 //			System.out.println(command);
-			
+
 			if (command.length() == 0) {
 				continue;
 			}
-			
+
 			if (command.equals("system exit")) {
 				break;
 			}
-			
-			if (command.equals("write")) {
-				lastArticleId++;
 
+			if (command.equals("write")) {
+				int id = ++lastArticleId;
+				lastArticleId = id;
 				System.out.print("제목 : ");
-				String title = sc.nextLine().trim();				
+				String title = sc.nextLine().trim();
 				System.out.print("내용 : ");
 				String body = sc.nextLine();
+
+				Article article = new Article(id, title, body);
+				articles.add(article);
+
+				System.out.printf("%d번째 글이 생성되었습니다.\n", id);
+			} else if (command.equals("list")) {
+
+				if (articles.size() == 0) {
+					System.out.println("게시물이 없습니다.");
+					continue;
+				}
 				
-				System.out.printf("%d번째 글이 생성되었습니다.", lastArticleId);
-			}
-			else if (command.equals("list")) {
-				System.out.printf("게시물이 %d개 있습니다.", lastArticleId);
-			}
-			else {
+				System.out.println("번호  /  제목");
+				
+				for (int i = articles.size()-1; i>=0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d  /  %s\n",article.id, article.title);
+				}
+
+			} else {
 				System.out.printf("%s는 존재하지 않는 명령어입니다.\n", command);
 				System.out.println("\n=== 명령어 목록 ===");
-				System.out.printf("write : 게시물작성\nlist : 게시물갯수\nsystem exit : 종료");
+				System.out.printf("write : 게시물작성\nlist : 게시물갯수\nsystem exit : 종료\n\n");
 			}
-			
+
 		}
-		
+
 		sc.close();
 
 		System.out.println("== 프로그램 끝 ==");
+	}
+}
+
+class Article {
+
+	int id;
+	String title;
+	String body;
+
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
 }
